54aad301ded5e3fee7f6feaa0b834726195b8032
jizhipeng111 <1565814702@qq.com>
2018-12-05 07:25:29 +0800

    delete network

--- network/network/dijkstra.h
+++
@@ -1,5 +0,0 @@
-#ifndef DIJKSTRA_H
-#define DIJKSTRA_H
-void dijkstra(int node, int **w); //找到最短路径生成树，并将路由器的路由表信息存入到链表中并输出
-#endif // !DIJKSTRA
-
--- network/read.h
+++
@@ -1,5 +0,0 @@
-#ifndef READ_H
-#define READ_H
-int** read(int node); //从文件中读取网络拓扑存入到一个邻接矩阵中,返回二维数组的首地址
-#endif // !READ_H
-
